4b26d55c76960cc717ba9eb511ad844f5a29cf2b
Marisha <galatea996@gmail.com>
2022-05-30 12:37:24 +0300

    new file:   Hello.sh

---
+++ Hello.sh
@@ -0,0 +1,26 @@
+##!/bin/bash
+pwd
+mkdir foldername
+cd foldername
+pwd
+mkdir papka
+cd papka
+mkdir Test1
+mkdir Test2
+mkdir Test3
+cd Test1
+mkdir Test4
+mkdir Test5
+mkdir Test6
+touch file1.txt
+touch file2.txt
+touch file3.txt
+touch filej1.json
+touch filej2.json
+touch filej3.json
+ls -a
+vim file2.txt
+mv file1.txt Test4
+mv filej1.json Test4
+cat file2.txt
+Date
